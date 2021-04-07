ed53b8547840d3d40a4608bac749fc95ed3018ad
James Brown <brownj95@gmail.com>
2021-04-07 23:55:34 +0000

    implemented page fetcher

---
+++ fetcher.js
@@ -0,0 +1,30 @@
+const args = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+
+const requestPage = function(functionToRunAfter) {
+  
+  request(args[0], (error, response, body) => {
+    if (error) {
+      throw new Error(`error`);
+    }
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    functionToRunAfter(body);
+  });
+
+};
+
+const writeFile = function(content) {
+
+  fs.writeFile(args[1], content, (error) => {
+    
+    if (error) {
+      throw new Error(`error`);
+    }
+    console.log(`Downloaded and saved to ${args[1]}`);
+  });
+
+};
+
+requestPage(writeFile);
